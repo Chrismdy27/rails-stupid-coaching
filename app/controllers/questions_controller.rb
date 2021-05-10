1e6fb8db617ff41ca823d9b3b824a37b297a1c9f
christophe mendy <christophe.mendy@kedgebs.com>
2021-05-10 13:46:00 +0200

    exo3

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @coachAnswer = params[:ask]
+    @answers = params[:question]
+    if @answers == "I am going to work"
+      return @coachAnswer = "Great!"
+    elsif @answers.end_with?("?") 
+      return @coachAnswer = "Silly question, get dressed and go to work!"
+    else 
+        @coachAnswer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
